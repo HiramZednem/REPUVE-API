063440b782c309ad885eaeeb03324774c831fc3c
KristellPerezMateos <213476@ids.upchiapas.edu.mx>
2022-10-09 16:47:59 -0500

    ADD entity

--- src/main/java/com/escuelita/demo/entities/Agency.java
+++ src/main/java/com/escuelita/demo/entities/Agency.java
@@ -1,5 +1,20 @@
 package com.escuelita.demo.entities;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Getter @Setter
 public class Agency {
-    //star
+@Id
+@GeneratedValue (strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String address;
+    private Long cellphone_number;
 }
